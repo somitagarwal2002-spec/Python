f3ab65a9da98c9b0a03802058d4d29941f2befbf
Somit Agarwal <somitagarwal@MacBookAir.lan>
2026-03-19 13:19:37 +0530

    commit

--- Day 25/50_states_game.py
+++ Day 25/50_states_game.py
@@ -20,23 +20,47 @@ turtle.shape(image)
 
 # screen.exitonclick()
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name").capitalize()
-# print(answer_state)
-
 data = pd.read_csv("Day 25/50_states.csv")
-# print(data)
+all_states = data.state.to_list()
+guessed_state =[]
+missing_state = []
+
+while len(guessed_state) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_state)}/50 States Guessed", 
+                                    prompt="What's another state's name").title()
+    print(answer_state)
+
+    if answer_state == "Exit":
+        for state in all_states:
+            if state not in guessed_state:
+                missing_state.append(state)
+        # print(missing_state)
+        new_data = pd.DataFrame(missing_state)
+        new_data.to_csv("Day 25/states_to_learn.csv")
+        break
+
+    if answer_state in all_states:
+        guessed_state.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(state_data.x.item(), state_data.y.item())
+        t.write(state_data.state.item()) # OR t.write(answer_state)
+
+
 count = 0
+screen.exitonclick()
+# def correct_guess():
+#     count += 1
+#     answer_state = screen.textinput(title=f"{count}/50 states correct", prompt="What's another state's name").capitalize()
 
-def correct_guess():
-    count += 1
-    answer_state = screen.textinput(title=f"{count}/50 states correct", prompt="What's another state's name").capitalize()
-
-for i in range(0, 100):
-    if answer_state in data["state"]:
-        count += 1
-        answer_state = screen.textinput(title=f"{count}/50 states correct", prompt="What's another state's name").capitalize()      
-    else:
-        answer_state = screen.textinput(title=f"{count}/50 states correct", prompt="What's another state's name").capitalize()
+# for i in range(0, 100):
+#     if answer_state in data["state"]:
+#         count += 1
+#         answer_state = screen.textinput(title=f"{count}/50 states correct", prompt="What's another state's name").capitalize()      
+#     else:
+#         answer_state = screen.textinput(title=f"{count}/50 states correct", prompt="What's another state's name").capitalize()
 
 
 
