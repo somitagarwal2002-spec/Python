527adf892142212640014887b22afe93b76c7467
somitagarwal2002-spec <somitagarwal2002@gmail.com>
2026-06-10 15:01:33 +0530

    commit

--- Day 57/challenge_1.py
+++ Day 57/challenge_1.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from flask import render_template
-
-AGIFY_API = "https://api.agify.io"
-GENDERIZE_API = "https://api.genderize.io"
+import requests
 
 app = Flask(__name__)
 
@@ -12,7 +10,17 @@ def home():
 
 @app.route('/guess/<name>')
 def work(name):
-    return render_template("gender_age.html", username=name)
+    age_url = f"https://api.agify.io/?name={name}"
+    age_response = requests.get(age_url)
+    age_data = age_response.json()
+    ages = age_data["age"]
+
+    gender_url = f"https://api.genderize.io/?name={name}"
+    gender_response = requests.get(gender_url)
+    gender_data = gender_response.json()
+    genders = gender_data["gender"]
+    
+    return render_template("gender_age.html", username=name, username_gender=genders["gender"], username_age=ages["age"])
 
 if __name__ == "__main__":
     app.run(debug=True)
