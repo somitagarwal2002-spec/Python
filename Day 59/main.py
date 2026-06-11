a037072a6547becb4b91a55af91c30c7a8f931f2
somitagarwal2002-spec <somitagarwal2002@gmail.com>
2026-06-11 20:08:19 +0530

    commit

---
+++ Day 59/main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+n_point_url = "https://api.npoint.io/5868ee9f7f74bbb450ce"
+posts = requests.get(n_point_url).json()
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
+
+@app.route('/about.html')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact.html')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/post.html/<int:index>')
+def post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+if __name__=="__main__":
+    app.run(debug=True)
